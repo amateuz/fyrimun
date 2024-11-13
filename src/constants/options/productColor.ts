b561de0a5f6e0a454821842a346a6d08c8cf2188
Serge <cache4moto@gmail.com>
2024-11-14 01:29:42 +0400

    chore: deleted unused files

--- src/constants/options/productColor.ts
+++
@@ -1,8 +0,0 @@
-import type { Option } from '@/types'
-
-const colorOptions: Option[] = [
-  { label: 'Shaded Blue', value: 'shaded-blue' },
-  { label: 'White', value: 'white' },
-  { label: 'Black', value: 'black' }
-]
-export default colorOptions
