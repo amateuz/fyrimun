158d1c7bb0c3f0d3638c8c4b4d3169318e736dac
Serge <cache4moto@gmail.com>
2024-11-13 19:21:53 +0400

    fix: router scroll to top added

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,7 +31,14 @@ routes.push(
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
